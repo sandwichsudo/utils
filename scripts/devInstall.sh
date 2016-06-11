db2edaf4f72d380cc8f756ef37b72e0e62c1dcde
Gilly Ames <gilly.ames@gmail.com>
2016-06-11 18:08:53 -0400

    Updating following upgrade to Ubuntu 16

--- scripts/devInstall.sh
+++ scripts/devInstall.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 sudo apt-get update  # To get the latest package lists
-sudo apt-get install curl -y
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash
-sudo apt-get install npm nodejs -y
+sudo apt-get install nodejs
+sudo apt-get install vim git npm -y
 #etc.
