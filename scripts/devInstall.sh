c5c74e7e682a71a724c56b38807d2e6dc45b7492
Gilly Ames <gilly.ames@gmail.com>
2016-06-11 20:49:42 +0100

    Adding fetch for node scr

--- scripts/devInstall.sh
+++ scripts/devInstall.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 sudo apt-get update  # To get the latest package lists
-sudo apt-get install npm node -y
+sudo apt-get install curl -y
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash
+sudo apt-get install npm nodejs -y
 #etc.
